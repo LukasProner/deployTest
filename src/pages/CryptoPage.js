5e8d4792e841991c69a20bbe548288238dc5c73d
unknown <lukasproner12@gmail.com>
2025-06-01 07:20:04 +0200

    pridala page

---
+++ src/pages/CryptoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CryptoPage() {
+  return (
+    <div>
+      <h1>Crypto stránka</h1>
+      <p>Toto je hlavná stránka tvojej aplikácie.</p>
+    </div>
+  );
+}
+
+export default CryptoPage;
